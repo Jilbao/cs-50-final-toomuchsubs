72f8407a5207920cfdbc9e522d632b2478b92c89
Çağın Yarkın Sarı <cyarkinsari@gmail.com>
2022-09-03 10:28:01 +0300

    DB connection, Session

--- app.js
+++ app.js
@@ -1,31 +1,82 @@
 //Requiries
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+//App Setup
 const app = express();
 const port = 3000;
-//EJS
-const ejs = require("ejs");
 
 //Static
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
-//Listen
-app.listen(port, () => {
-    console.log("Server is up! Port: 3000");
+//Session
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Database
+mongoose.connect(process.env.DB_URL);
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+})
+UserSchema.plugin(passportLocalMongoose);
+const User = mongoose.model("user", UserSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+ 
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
 //Index
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.render("home");
 });
 
 //Login
-app.get("/login", (req, res) => { 
-    res.sendFile(__dirname + "/login.html");
-});
+app.route("/login")
+   .get((req, res)=>{
+        res.render("login");
+   })
+   .post((req, res)=>{
+
+   })
+
 
 //Register
-app.get("/register", (req, res) => {
-    res.sendFile(__dirname + "/register.html");
+
+app.route("/register")
+   .get((req, res)=>{
+        res.render("register");
+   })
+   .post((req, res)=>{
+
+   });
+
+
+
+//Listen
+app.listen(port, () => {
+    console.log("Server is up! Port: 3000");
 });
